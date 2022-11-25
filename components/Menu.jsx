3636e1fdd029cdc4f18c9ab665fce6ed1033bdf6
arifkhan1990 <arifkhanshubro@gmail.com>
2022-11-26 04:14:37 +0600

    create dynamic route and create product deatils page size and quantity increment and dicrement funcationality

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,6 +1,7 @@
 import css from '../styles/Menu.module.css';
 import Image from 'next/image';
 import { urlFor } from '../lib/client';
+import Link from 'next/Link';
 export default function Menu({pizzas}){
     // console.log(pizzas);
     return(
@@ -10,19 +11,28 @@ export default function Menu({pizzas}){
                 <span>Menu That Always</span>
                 <span>Make you Fall In Love</span>
             </div>
+            <div className={css.menu}>
+
             {/* pizzas */}
-            {pizzas.map((pizza,id)=> {
-                const src = urlFor(pizza.image).url();
-                return(
-                    <div className={css.pizza} key={id}>
-                        <div className={css.ImageWrapper}>
-                            <Image 
-                            loader={()=> src}
-                            src={src} alt="" objectFit='cover' layout='fill'/>
+                {pizzas.map((pizza,id)=> {
+                    const src = urlFor(pizza.image).url();
+                    return(
+                        <div className={css.pizza} key={id}>
+                            <Link href={`./pizza/${pizza.slug.current}`}>
+
+                            <div className={css.ImageWrapper}>
+                                <Image 
+                                loader={()=> src}
+                                src={src} alt="" objectFit='cover' layout='fill'/>
+                            </div>
+                            </Link>
+                            <span>{pizza.name}</span>
+                            <span><span style={{color:'var(--dollarRed)'}}>$</span>{pizza.price[1]}</span>
+
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     )
 }
