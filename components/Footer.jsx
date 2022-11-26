73d369752d2105479a4135508d6c4fa61dd74073
arifkhan1990 <arifkhanshubro@gmail.com>
2022-11-27 00:59:57 +0600

    footer content changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,21 +2,25 @@ import css from '../styles/Footer.module.css';
 import {UilFacebook, UilGithub, UilTwitter, UilInstagram} from "@iconscout/react-unicons";
 import Image from "next/image";
 import Logo from "../assets/Logo.png";
+import Link from 'next/link';
 export default function Footer() {
     return(
-        <div className={css.container}>
-            <span>ALL RIGHT RESERVED</span>
+        <div className={css.header}>
+            {/* LOG SIDE */}
             <div className={css.social}>
-                <UilFacebook size={45}/>
-                <UilTwitter size={45}/>
-                <UilGithub size={45}/>
-                <UilInstagram size={45}/>
-            </div> 
-            <div className={css.logo}>
-                <Image src={Logo} alt="" width={50} height={50} />
-                <span>Fariwala Pizza Shop</span>
-            </div> 
+                    <Link href='https://www.facebook.com/arif.khan.shubro/'><a target='_blank'><UilFacebook size={35}/></a></Link>
+                    <Link href=''><a target='_blank'><UilTwitter size={35}/></a></Link>
+                    <Link href='https://github.com/arifkhan1990'><a target='_blank'><UilGithub size={35}/></a></Link>
+                    <Link href='https://www.instagram.com/arif.khan.1990/'><a target='_blank'><UilInstagram size={35}/></a></Link>
+                </div> 
+
+            {/* right side */}
+            <div className={css.rightSide}>
+                <div className={css.reservedTit}>
+                    <span><Link href='http://entertechbd.com/'><a target='_blank'><span style={{color:"var(--dollarRed)"}}>AGAMIRIT.SOLUTIONS</span></a></Link> &copy; All right reserved.</span>
+                </div>
+            </div>
+
         </div>
     )
-    
 };
